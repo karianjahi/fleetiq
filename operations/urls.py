504b53c2f912bc90589879486a07e0a237422fb5
karianjahi <joseph@spiced-academy.com>
2026-05-26 16:16:19 +0200

    Add a dashboard url endpoint

--- operations/urls.py
+++ operations/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("api/dashboard/kpis/", views.dashboard_kpis, name="dashboard-kpis"),
     path("api/alerts/latest/", views.latest_alerts, name="latest-alerts"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
 
 
