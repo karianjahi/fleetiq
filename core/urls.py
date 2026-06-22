870ef29417d7ab7ac7eecd1ac5503c0c0bab805f
karianjahi <joseph@spiced-academy.com>
2026-06-22 12:08:35 +0200

    feat: use single quotes on root url for consistency

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("operations.urls")),
-    path("", lambda request: redirect("dashboard")),
+    path('', lambda request: redirect("dashboard")),
 ]
