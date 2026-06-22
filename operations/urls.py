f9b879cae379a88f6d625098186a7d1e38a0f312
karianjahi <joseph@spiced-academy.com>
2026-06-22 12:18:37 +0200

    feat: add landing page url

--- operations/urls.py
+++ operations/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.dashboard_view, name="dashboard"),
     
+    path("", views.landing_page, name="landing_page"),
+    
     path("api/dashboard/kpis/", views.dashboard_kpis, name="dashboard-kpis"),
     
     path("api/alerts/latest/", views.latest_alerts, name="latest-alerts"),
