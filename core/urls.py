5fc80e75c3bcd6e8f70773927b6b848010e852bc
karianjahi <joseph@spiced-academy.com>
2026-06-22 12:07:52 +0200

    feat: redirect the homepage to dashboard

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("operations.urls")),
+    path("", lambda request: redirect("dashboard")),
 ]
